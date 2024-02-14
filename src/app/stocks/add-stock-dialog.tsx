9eebb3f50ae2db5256bc69bb7147cf77ba333e53
Thiago Elias <thiagoelias99@gmail.com>
2024-02-14 20:59:38 -0300

    feat: :sparkles: Adding stocks dialog

--- src/app/stocks/add-stock-dialog.tsx
+++ src/app/stocks/add-stock-dialog.tsx
@@ -1,4 +1,18 @@
+import { Button } from "@/components/ui/button"
 import { DialogHeader, Dialog, DialogContent, DialogTitle, DialogDescription } from "@/components/ui/dialog"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { z } from "@/lib/pt-zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+  ticker: z.string().min(2).max(10),
+  orderType: z.enum(["buy", "sell"]),
+  quantity: z.number().min(1),
+  price: z.number().min(0.01),
+})
 
 interface Props {
   open: boolean
@@ -6,16 +20,44 @@ interface Props {
 }
 
 export default function AddStockDialog({ onOpenChange, open }: Props) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your account
-            and remove your data from our servers.
-          </DialogDescription>
+          <DialogTitle>Adicione uma nova ordem</DialogTitle>
         </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   )
