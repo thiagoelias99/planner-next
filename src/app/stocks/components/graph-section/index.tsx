168000b0f31a0e0911cef6f556bfe1ded9e847d3
Thiago Elias <thiagoelias99@gmail.com>
2024-04-04 11:05:19 -0300

    feat: :sparkles: Configured Graph

--- src/app/stocks/components/graph-section/index.tsx
+++ src/app/stocks/components/graph-section/index.tsx
@@ -1,10 +1,47 @@
+'use client'
+
 import { Card } from '@/components/ui/card'
-import React from 'react'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, Colors, ChartData } from 'chart.js'
+import { Doughnut } from 'react-chartjs-2'
 
 export const GraphSection = () => {
+  ChartJS.register(ArcElement, Tooltip, Legend, Colors)
+
+  const data = {
+    labels: [
+      'Ações',
+      'FIIs',
+      'Internacional',
+      'Cripto',
+      'Ouro'
+    ],
+    datasets: [{
+      label: 'Porcentagem',
+      data: [300, 50, 100, 30, 40],
+      hoverOffset: 4
+    }]
+  }
+
   return (
     <Card className='m-4 h-[359px] border-2 flex items-center justify-center'>
-      <h1>Graph</h1>
+      <div>
+        <Doughnut data={data}
+          options={{
+            plugins: {
+              legend: {
+                position: 'bottom',
+                labels: {
+                  color: 'white',
+                  font: {
+                    size: 16,
+                    family: 'Roboto'
+                  }
+                }
+              }
+            }
+          }}
+        />
+      </div>
     </Card>
   )
 }
