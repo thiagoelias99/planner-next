8c3eba05671b745c3ed7f6c292e9e66fa8fe7612
Thiago Elias <thiagoelias99@gmail.com>
2024-04-08 13:46:25 -0300

    feat: :lipstick: Add Icon

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 // import logo from '../assets/logo.jpeg'
 import SignUpForm from './signup-form'
 import LoginForm from './login-form'
+import Image from 'next/image'
 
 
 const LoginPage = () => {
@@ -11,8 +12,18 @@ const LoginPage = () => {
 
   return (
     <main className='w-screen h-screen flex flex-col'>
-      <div className={`w-full ${showSignUp ? 'h-[64px]' : 'h-[30%]'} bg-primary flex items-center justify-center`}>
+      <div className={`relative w-full ${showSignUp ? 'h-[64px]' : 'h-[30%]'} bg-primary flex items-center justify-center`}>
         {/* <img src={logo} alt="logo" className={showSignUp ? 'h-[100%]' : 'h-[80%]'} /> */}
+
+        <div className={showSignUp ? 'h-[100%]' : 'h-[80%]'}>
+          <Image
+            src="/logo-1024.png"
+            alt="App logo"
+            fill
+            objectFit='contain'
+          />
+        </div>
+
       </div>
       <div className='w-full sm:max-w-[460px] sm:m-auto flex-1 bg-background flex flex-col items-center justify-start'>
         {showSignUp ? <SignUpForm setShowSignUp={setShowSignUp} /> : <LoginForm />}
