137abdf82892e2b69c4317b0640ed8189111730f
Thiago Elias <thiagoelias99@gmail.com>
2024-07-18 09:17:22 -0300

    build

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,6 @@ const nextConfig = {
   i18n: {
     locales: ['pt-BR'],
     defaultLocale: 'pt-BR',
-  },
-  generateBuildId: async () => {
-    // This could be anything, using the latest git hash
-    return process.env.AUTH_SECRET
   }
 }
 
